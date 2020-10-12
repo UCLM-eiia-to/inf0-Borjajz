76f2bb3506fcd2dce6a0f96b9e106d16f5c1099b
Borjajz <72757888+Borjajz@users.noreply.github.com>
2020-10-12 21:25:36 +0200

    Update correo.py

--- correo.py
+++ correo.py
@@ -1,32 +1,5 @@
 # Define aquí mismo la función my_email sin argumentos que 
 # devuelve tu correo electrónico de la UCLM
-my_email="Borja.Jerez@alu.uclm.es"
-my_email
-import unittest
-class Test(unittest.TestCase):
+def my_email()=
+return "Borja.Jerez@alu.uclm.es"
 
-    def test_01_devuelve_una_cadena(self):
-        from correo import  my_email
-        self.assertEqual(str,type(my_email()))
-
-    def test_02_contiene_una_arroba(self):
-        from correo import  my_email
-        self.assertEqual(1,my_email().count('@'))
-
-    def test_03_es_direccion_de_alumno(self):
-        from correo import  my_email
-        self.assertTrue(my_email().endswith('@alu.uclm.es'))
-
-    def test_04_no_contiene_espacios(self):
-        from correo import  my_email
-        email = my_email()
-        self.assertFalse(' ' in email)
-        self.assertFalse('\t' in email)
-        self.assertFalse('\n' in email)
-        self.assertFalse('\r' in email)
-
-if __name__ == "__main__":
-    # execute only if run as a script
-    unittest.main()
-    
-  
